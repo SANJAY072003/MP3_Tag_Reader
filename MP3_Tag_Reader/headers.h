79b209902fba9fa27f5fdffc4252f07bc5036a70
Sanjay kumar S <sanjaykumar0710203@gmail.com>
2025-11-28 19:33:31 +0530

    Add files via upload

---
+++ MP3_Tag_Reader/headers.h
@@ -0,0 +1,13 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+void edit(FILE *,char, char *, char *);
+void copy(FILE *ftr, FILE *, char *arr, char *, char *);
+void view(FILE *);
+void swap(char *);
+int search(FILE *, char *, char *);
+void print(int , char *);
+void error_msg(void);
+
+
+#endif
